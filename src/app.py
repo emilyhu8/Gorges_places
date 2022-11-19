4e556a662ca84a41716ff2c45e928d8b88c5f615
Emily Hu <exh2@cornell.edu>
2022-11-18 20:05:16 -0500

    added some app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 import json
 from flask import request
-from db import db
+from db import db, Place, Activity, Review
 import os
 
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "gp.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -15,3 +15,51 @@ app.config["SQLALCHEMY_ECHO"] = True
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+@app.route("/api/places/")
+def get_places():
+    """
+    Endpoint for getting all places
+    """
+    places = [place.serialize() for place in Place.query.all()]
+    return success_response({"places": places})
+
+@app.route("/api/place/<int:place_id>/")
+def get_place(place_id):
+    """
+    Endpoint for getting a place by id
+    """
+    place=Place.query.filter_by(id=place_id).first()
+    if place is None:
+        return failure_response("Place is not found")
+    return success_response(place.serialize())
+
+@app.route("/api/places/", methods=["POST"])
+def create_place():
+    """
+    Endpoint for creating a new place
+    """
+    body=json.loads(request.data)
+    if not body.get("name") or not body.get("description"):
+        return failure_response("Please supply all fields.", 400)
+    new_place = Place(name=body.get("name"), description=body.get("description"))
+    db.session.add(new_place)
+    db.session.commit()
+    return success_response(new_place.serialize(), 201)
+
+@app.route("/api/activities/")
+def get_activities():
+    """
+    Endpoint for getting all activities
+    """
+    activities = [activity.serialize() for activity in Activity.query.all()]
+    return success_response({"activities": activities})
+
+
