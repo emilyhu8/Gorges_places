0408923a516170e4cef1fa9a35fc7a9d8900b87b
Emily Hu <exh2@cornell.edu>
2022-11-30 20:28:11 -0500

    completed backend just need to add the places

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import json
 from flask import request
-from db import db, Place, Activity
+from db import db, Place, Activity, PlaceReview, Category
 import os
 
 
@@ -23,6 +23,14 @@ def success_response(data, code=200):
 def failure_response(message, code=404):
     return json.dumps({"error": message}), code
 
+@app.route("/api/categories/")
+def get_categories():
+    """
+    Endpoint for getting all categories
+    """
+    categories = [category.serialize() for category in Category.query.all()]
+    return success_response({"categories": categories})
+
 @app.route("/api/places/")
 def get_places():
     """
@@ -34,32 +42,65 @@ def get_places():
 @app.route("/api/place/<int:place_id>/")
 def get_place(place_id):
     """
-    Endpoint for getting a place by id
+    Endpoint for getting a place by place id
     """
     place=Place.query.filter_by(id=place_id).first()
     if place is None:
         return failure_response("Place is not found")
     return success_response(place.serialize())
 
-@app.route("/api/places/", methods=["POST"])
-def create_place():
+@app.route("/api/<int:category_id>/places/")
+def get_places_by_category(category_id):
     """
-    Endpoint for creating a new place
+    Endpoint for getting all places for a category
+    """
+    category=Category.query.filter_by(id=category_id).first()
+    if category is None: 
+        return failure_response ("Category can't be found")
+    places = [place.serialize() for place in Place.query.filter_by(category_id=category_id)]
+    return success_response({"places": places})
+
+@app.route("/api/categories/", methods=["POST"])
+def create_category():
+    """
+    Endpoint to create a category
     """
     body=json.loads(request.data)
-    if not body.get("name") or not body.get("description"):
-        return failure_response("Please supply all fields.", 400)
-    new_place = Place(name=body.get("name"), description=body.get("description"))
+    name=body.get("name")
+    if name is None:
+        return failure_response ("Please fill out all fields.", 400)
+    new_category=Category(name=name)
+    db.session.add(new_category)
+    db.session.commit()
+    return success_response (new_category.serialize(), 201)
+
+@app.route("/api/<int:category_id>/place/", methods=["POST"])
+def create_place(category_id):
+    """
+    Endpoint to create a place
+    """
+    category=Category.query.filter_by(id=category_id).first()
+    if category is None: 
+        return failure_response ("Category can't be found")
+    body=json.loads(request.data)
+    name=body.get("name")
+    description=body.get("description")
+    if name is None or description is None:
+        return failure_response ("Please fill out all fields.", 400)
+    new_place=Place(name=name, description=description, category_id=category_id)
     db.session.add(new_place)
     db.session.commit()
-    return success_response(new_place.serialize(), 201)
+    return success_response (new_place.serialize(), 201)
 
-@app.route("/api/activities/")
-def get_activities():
+@app.route("/api/<int:place_id>/activity/")
+def get_activities(place_id):
     """
-    Endpoint for getting all activities
+    Endpoint for getting all activities for a place
     """
-    activities = [activity.serialize() for activity in Activity.query.all()]
+    place=Place.query.filter_by(id=place_id).first()
+    if place is None: 
+        return failure_response ("Place can't be found")
+    activities = [activity.serialize() for activity in Activity.query.filter_by(place_id=place_id)]
     return success_response({"activities": activities})
 
 @app.route("/api/places/<int:place_id>/activity/", methods=["POST"])
@@ -68,19 +109,62 @@ def create_activity(place_id):
     Endpoint to create an activity to a place
     """
     place=Place.query.filter_by(id=place_id).first()
+    if place is None: 
+        return failure_response ("Place can't be found")
     body=json.loads(request.data)
     name=body.get("name")
     description=body.get("description")
+    completed=body.get("completed", False)
     if name is None or description is None:
         return failure_response ("Please fill out all fields.", 400)
-    new_activity=Activity(name=name, description=description, course_id=place_id)
-    if place is None:
-      new_activity=Activity(name=name, description=description)
-    else:
-      new_activity=Activity(name=name, description=description, place_id=place_id)
+    new_activity=Activity(name=name, description=description, completed= completed, place_id=place_id)
     db.session.add(new_activity)
     db.session.commit()
     return success_response (new_activity.serialize(), 201)
+
+@app.route("/api/activity/<int:activity_id>/", methods=["DELETE"])
+def delete_activity(activity_id):
+    """
+    Endpoint for deleting a specific activity by id
+    """
+    activity=Activity.query.filter_by(id=activity_id).first()
+    if activity is None:
+        return failure_response("Activity is not found")
+    db.session.delete(activity)
+    db.session.commit()
+    return success_response(activity.serialize())
+
+@app.route("/api/places/<int:place_id>/review/")
+def get_reviews(place_id):
+    """
+    Endpoint to get reviews for a specific place
+    """
+    place=Place.query.filter_by(id=place_id).first()
+    if place is None:
+        return failure_response ("Place can't be found")
+    reviews = [review.serialize() for review in PlaceReview.query.filter_by(place_id=place_id)]
+    return success_response({"reviews": reviews})
+    
+@app.route("/api/places/<int:place_id>/review/", methods=["POST"])
+def create_review(place_id):
+    """
+    Endpoint to create a review to a place
+    """
+    place=Place.query.filter_by(id=place_id).first()
+    if place is None: 
+        return failure_response ("Place can't be found")
+    body=json.loads(request.data)
+    username=body.get("username")
+    rating=body.get("rating")
+    if username is None or rating is None:
+        return failure_response ("Please fill out all fields.", 400)
+    new_review=PlaceReview(username=username,rating=rating, place_id=place_id)
+    db.session.add(new_review)
+    db.session.commit()
+    return success_response (new_review.serialize(), 201)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8008, debug=True)
 
 
 
--- src/db.py
+++ src/db.py
@@ -2,27 +2,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-place_association_table= db.Table(
-  "place_association_table",
-  db.Column("category_id", db.Integer, db.ForeignKey("category.id")),
-  db.Column("activity_id", db.Integer, db.ForeignKey("place.id")) 
-)
+class Category(db.Model):
+  __tablename__="category"
+  id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+  name=db.Column(db.String, nullable=False)
+  places=db.relationship("Place", cascade="delete")
+  
+  def __init__ (self, **kwargs):
+    self.name=kwargs.get("name", "")
+  
+  def serialize (self):
+    return {
+      "id": self.id, 
+      "name": self.name,
+      "places": [p.simple_serialize() for p in self.places],     
+    }
 
-activity_association_table= db.Table(
-  "activity_association_table",
-  db.Column("category_id", db.Integer, db.ForeignKey("category.id")),
-  db.Column("activity_id", db.Integer, db.ForeignKey("activity.id")) 
-)
+  def simple_serialize(self):
+    return {
+      "id": self.id, 
+      "name": self.name, 
+    }
 
 class Place (db.Model):
   __tablename__="place"
   id=db.Column(db.Integer, primary_key=True, autoincrement = True)
   name = db.Column(db.String, nullable = False)
   description = db.Column (db.String, nullable = False)
-  category=db.relationship("Category", secondary = place_association_table, back_populates=True)
+  category_id=db.Column(db.String, db.ForeignKey("category.id"))
   activities = db.relationship ("Activity", cascade="delete")
   ratings = db.relationship ("PlaceReview", cascade= "delete")
-  comments = db.relationship ("PlaceReview", cascade ="delete")
 
   def __init__ (self, **kwargs):
     """
@@ -30,6 +39,7 @@ class Place (db.Model):
     """
     self.name = kwargs.get("name", "")
     self.description = kwargs.get ("description", "")
+    self.category_id=kwargs.get("category_id", "")
   
   def serialize (self):
     """
@@ -39,10 +49,9 @@ class Place (db.Model):
       "id": self.id, 
       "name": self.name, 
       "description": self.description, 
-      "category": [c.simple_serialize() for c in self.category],
+      "category": (Category.query.filter_by(id=self.category_id).first()).simple_serialize(),
       "activities": [a.simple_serialize() for a in self.activities], 
       "ratings": [r.simple_serialize() for r in self.ratings], 
-      "comments": [c.simple_serialize() for c in self.comments] 
     }
   
   def simple_serialize(self):
@@ -60,11 +69,8 @@ class Activity (db.Model):
   id=db.Column(db.Integer, primary_key = True, autoincrement = True)
   name=db.Column(db.String, nullable=False)
   description=db.Column(db.String, nullable=False)
-  category=db.relationship("Category", secondary = activity_association_table, back_populates=True)
-  completed = db.Column(db.Boolean, nullable =True)
-  place_id = db.Column(db.String, db.ForeignKey("place.id"), nullable=True)
-  ratings = db.relationship ("ActivityReview", cascade= "delete")
-  comments = db.relationship ("ActivityReview", cascade ="delete")
+  completed = db.Column(db.Boolean, nullable =False)
+  place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
 
   def __init__ (self, **kwargs):
     """
@@ -72,7 +78,8 @@ class Activity (db.Model):
     """
     self.name=kwargs.get("name", "")
     self.description=kwargs.get("description", "")
-    self.completed=kwargs.get("completed", False)
+    self.completed=kwargs.get("completed", "")
+    self.place_id=kwargs.get("place_id", "")
 
   def serialize(self):
     """
@@ -82,11 +89,8 @@ class Activity (db.Model):
       "id": self.id, 
       "name": self.name, 
       "description": self.description,
-      "category": [c.simple_serialize() for c in self.category], 
       "complete": self.completed,
-      "place": (Place.query.filter_by(id=self.place_id)).simple_serialize(), 
-      "ratings": [r.simple_serialize() for r in self.ratings], 
-      "comments": [c.simple_serialize() for c in self.comments] 
+      "place": (Place.query.filter_by(id=self.place_id).first()).simple_serialize(), 
     }
   
   def simple_serialize(self):
@@ -105,8 +109,7 @@ class PlaceReview(db.Model):
   id=db.Column(db.Integer, primary_key =True, autoincrement = True)
   username=db.Column(db.String, nullable=False)
   rating = db.Column(db.Integer, nullable=False)
-  comment = db.Column(db.String, nullable= True)
-  place_id = db.Column(db.String, db.ForeignKey("place.id"), nullable=True)
+  place_id = db.Column(db.Integer, db.ForeignKey("place.id"))
 
   def __init__(self, **kwargs):
     """
@@ -114,7 +117,7 @@ class PlaceReview(db.Model):
     """
     self.username=kwargs.get("username", "")
     self.rating=kwargs.get("rating", "")
-    self.comment=kwargs.get("comment", "")
+    self.place_id=kwargs.get("place_id", "")
 
   def serialize(self):
     """
@@ -124,8 +127,6 @@ class PlaceReview(db.Model):
       "id": self.id, 
       "username": self.username, 
       "rating": self.rating, 
-      "comment": self.comment, 
-      "place": (Place.query.filter_by_id(id=self.place_id).first()).simple_serialize(), 
     }
   
   def simple_serialize(self):
@@ -135,69 +136,6 @@ class PlaceReview(db.Model):
     return {
       "username": self.username, 
       "rating": self.rating, 
-      "comment": self.comment
-    }
-
-class ActivityReview(db.Model):
-  __tablename__="activity_review"
-  id=db.Column(db.Integer, primary_key =True, autoincrement = True)
-  username=db.Column(db.String, nullable=False)
-  rating = db.Column(db.Integer, nullable=False)
-  comment = db.Column(db.String, nullable= True)
-  activity_id = db.Column(db.String, db.ForeignKey("activity.id"), nullable=True)
-
-  def __init__(self, **kwargs):
-    """
-    Creates a ActivityReview object
-    """
-    self.username=kwargs.get("username", "")
-    self.rating=kwargs.get("rating", "")
-    self.comment=kwargs.get("comment", "")
-
-  def serialize(self):
-    """
-    Serializes a ActivityReview object
-    """
-    return {
-      "id": self.id, 
-      "username": self.username, 
-      "rating": self.rating, 
-      "comment": self.comment, 
-      "activity": (Activity.query.filter_by_id(id=self.activity_id).first()).simple_serialize()
-    }
-  
-  def simple_serialize(self):
-    """
-    Simple Serializes a ActivityReview object
-    """
-    return {
-      "username": self.username, 
-      "rating": self.rating, 
-      "comment": self.comment
-    }
-
-class Category(db.Model):
-  __tablename__="category"
-  id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-  name=db.Column(db.String, nullable=False)
-  activities=db.relationship("Activity", secondary=activity_association_table, back_populates=True)
-  activities=db.relationship("Place", secondary=place_association_table, back_populates=True)
-
-  def __init__ (self, **kwargs):
-    self.name=kwargs.get("name", "")
-  
-  def serialize (self):
-    return {
-      "id": self.id, 
-      "name": self.name,
-      "places": [p.serialize() for p in self.places],
-      "activities": [a.serialize() for a in self.activities]     
-    }
-
-  def simple_serlialize(self):
-    return {
-      "id": self.id, 
-      "name": self.name, 
     }
 
 
